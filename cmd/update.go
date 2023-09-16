67678a94473d543df73e9674ab37765d22b7f199
aidanaden <aidan@u.nus.edu>
2023-09-17 05:02:54 +0800

    chore: set up basic commands

--- cmd/update.go
+++ cmd/update.go
@@ -1,5 +1,6 @@
 /*
 Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+
 */
 package cmd
 
@@ -9,9 +10,9 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// peopleCmd represents the people command
-var peopleCmd = &cobra.Command{
-	Use:   "people",
+// updateCmd represents the update command
+var updateCmd = &cobra.Command{
+	Use:   "update",
 	Short: "A brief description of your command",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
@@ -20,20 +21,20 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("people called")
+		fmt.Println("update called")
 	},
 }
 
 func init() {
-	viewCmd.AddCommand(peopleCmd)
+	rootCmd.AddCommand(updateCmd)
 
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
-	// peopleCmd.PersistentFlags().String("foo", "", "A help for foo")
+	// updateCmd.PersistentFlags().String("foo", "", "A help for foo")
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
-	// peopleCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// updateCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
