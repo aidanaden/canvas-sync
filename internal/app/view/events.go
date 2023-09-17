07d37e47d9684ff6035b4db6b11f234720d03d7f
aidanaden <aidan@u.nus.edu>
2023-09-18 00:09:53 +0800

    chore: add base files for all commands

--- internal/app/view/events.go
+++ internal/app/view/events.go
@@ -1,6 +1,11 @@
 package view
 
-import "github.com/spf13/cobra"
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
 
 func RunViewEvents(cmd *cobra.Command, args []string) {
+	fmt.Println("viewing events soon bro")
 }
