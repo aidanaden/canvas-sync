7a9364d64b8a33354b6566e00be7b32aa86fbc53
aidanaden <aidan@u.nus.edu>
2023-09-19 00:57:45 +0800

    chore: remove comment

--- scripts/completions.sh
+++ scripts/completions.sh
@@ -3,7 +3,6 @@
 set -e
 rm -rf completions
 mkdir completions
-# TODO: replace your-cli with your binary name
 for sh in bash zsh fish; do
 	go run main.go completion "$sh" >"completions/canvas-sync.$sh"
 done
