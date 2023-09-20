f43df912346c265ce4b41e8b98a37af71b0194d4
Ryan Aidan <aidan@u.nus.edu>
2023-09-21 04:14:48 +0800

    feat: user-configurable init (#86)

--- internal/app/initialise/config.go
+++ internal/app/initialise/config.go
@@ -1,23 +1,100 @@
 package initialise
 
 import (
-	"fmt"
+	"bytes"
 	"os"
 	"path/filepath"
+	"text/template"
 
 	"github.com/pterm/pterm"
 )
 
+type Config struct {
+	DataDir     string
+	CanvasUrl   string
+	AccessToken string
+}
+
+const cfg_template = `
+# file directory to store canvas data e.g. ~/.canvas-sync/data
+data_dir: {{ .DataDir }}
+# canvas website url e.g. https://canvas.nus.edu.sg
+canvas_url: {{ .CanvasUrl }}
+# your canvas access token
+access_token: {{ .AccessToken }}
+`
+
+func generate(config *Config) string {
+	t, err := template.New("yaml generator").Parse(cfg_template)
+	if err != nil {
+		pterm.Error.Printfln("Error generating yaml generator: %s", err.Error())
+	}
+	buf := &bytes.Buffer{}
+	err = t.Execute(buf, config)
+	if err != nil {
+		pterm.Error.Printfln("Error generating yaml from config: %s", err.Error())
+	}
+	return buf.String()
+}
+
 func initConfigFile(path string) {
 	configDir := filepath.Dir(path)
-	dataDir := filepath.Join(configDir, "data")
-	d1 := []byte(
-		fmt.Sprintf("# paste your access token below\naccess_token: \n# replace with your preferred location to store canvas data\ndata_dir: %s\n# replace with your own canvas url if not from nus\ncanvas_url: %s\n", dataDir, "https://canvas.nus.edu.sg"),
-	)
+	accessToken := ""
+	dataDir := ""
+	canvasUrl := ""
+	var err error
+
+	pterm.Println()
+	dataDir, err = pterm.DefaultInteractiveTextInput.WithMultiLine(false).Show("Enter location to store downloaded canvas data (default: ~/.canvas-sync/data)")
+	if err != nil {
+		pterm.Error.Printfln("Error getting input for data location: %s", err.Error())
+		os.Exit(1)
+	}
+	canvasUrl, err = pterm.DefaultInteractiveTextInput.WithMultiLine(false).Show("Enter canvas url (default: https://canvas.nus.edu.sg)")
+	if err != nil {
+		pterm.Error.Printfln("Error getting input for canvas url: %s", err.Error())
+		os.Exit(1)
+	}
+	accessToken, err = pterm.DefaultInteractiveTextInput.WithMultiLine(false).Show("Enter access token (optional, skip if u dont have one)")
+	if err != nil {
+		pterm.Error.Printfln("Error getting input for access token: %s", err.Error())
+		os.Exit(1)
+	}
+
+	// set defaults
+	if dataDir == "" {
+		dataDir = filepath.Join(configDir, "data")
+	}
+	if canvasUrl == "" {
+		canvasUrl = "https://canvas.nus.edu.sg"
+	}
+
+	d1 := []byte(generate(&Config{
+		DataDir:     dataDir,
+		CanvasUrl:   canvasUrl,
+		AccessToken: accessToken,
+	}))
+
 	if err := os.WriteFile(path, d1, 0755); err != nil {
 		pterm.Error.Printfln("Error creating config file: %s", err.Error())
 		os.Exit(1)
 	}
+
+	pterm.Println()
+	pterm.Success.Printfln("Successfully created config file: %s", path)
+	pterm.Println()
+	td := [][]string{
+		{pterm.FgCyan.Sprint("data_dir"), pterm.FgGreen.Sprint(dataDir)},
+		{pterm.FgCyan.Sprint("canvas_url"), pterm.FgGreen.Sprint(canvasUrl)},
+		{pterm.FgCyan.Sprint("access_token"), pterm.FgGreen.Sprint(accessToken)},
+	}
+	tablePrint, err := pterm.DefaultTable.WithHasHeader().WithData(td).Srender()
+	if err != nil {
+		pterm.Error.Printfln("Error rendering config table: %s", err.Error())
+		os.Exit(1)
+	}
+	box := pterm.DefaultBox.WithTitle(pterm.Sprintf("Config: %s", path)).Sprint(tablePrint)
+	pterm.Println(box)
 }
 
 var DEFAULT_CONFIG_DIR = ".canvas-sync"
@@ -58,10 +135,6 @@ func RunInit(isInitCommand bool) string {
 		}
 		if res {
 			initConfigFile(cfgFilePath)
-			if isInitCommand {
-				pterm.Println()
-				pterm.Success.Printfln("Successfully created config file: %s", cfgFilePath)
-			}
 		} else {
 			pterm.Println()
 			pterm.Error.Println("Init command cancelled.")
@@ -71,10 +144,6 @@ func RunInit(isInitCommand bool) string {
 			pterm.Warning.Println("No config file found, creating default config...")
 		}
 		initConfigFile(cfgFilePath)
-		if isInitCommand {
-			pterm.Println()
-			pterm.Success.Println("Successfully created config file.")
-		}
 	} else if err != nil {
 		pterm.Error.Printfln("Error getting config file info: %s", err.Error())
 		os.Exit(1)
--- internal/pkg/canvas/client.go
+++ internal/pkg/canvas/client.go
@@ -75,8 +75,7 @@ func (c *CanvasClient) ExtractStoredBrowserCookies() error {
 		}
 		subsplits := strings.Split(split, "=")
 		if len(subsplits) != 2 {
-			pterm.Error.Printfln("Invalid cookie %s, skipping...", split)
-			continue
+			return errors.New("no valid cookies found")
 		}
 		cookies = append(cookies, &http.Cookie{
 			Name:  subsplits[0],
@@ -91,6 +90,15 @@ func (c *CanvasClient) ExtractStoredBrowserCookies() error {
 	cookieJar.SetCookies(&baseUrl, cookies)
 	c.client.Jar = cookieJar
 	return nil
+}
+
+// extract stored cookies, if none found extract browser cookies
+func (c *CanvasClient) ExtractCookies() {
+	if err := c.ExtractStoredBrowserCookies(); err != nil {
+		pterm.Info.Printfln("No stored cookies found, using browser cookies...")
+		c.ExtractBrowserCookies()
+		c.StoreDomainBrowserCookies()
+	}
 }
 
 func (c *CanvasClient) StoreDomainBrowserCookies() {
