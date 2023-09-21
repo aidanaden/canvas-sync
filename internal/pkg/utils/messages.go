8bb6c45cf5c3391e55a921c032971abb84d1e642
Amir Khazaie <733amir@gmail.com>
2023-09-21 19:17:27 +0330

    add some emoji to the message

--- internal/pkg/utils/messages.go
+++ internal/pkg/utils/messages.go
@@ -3,6 +3,6 @@ package utils
 import "github.com/pterm/pterm"
 
 func MotivationalAndStarMessage() {
-	pterm.Info.Println("Good luck in school. You Got this.")
-	pterm.Info.Println("Thanks for using canvas-sync. Star us on GitHub: https://github.com/aidanaden/canvas-sync")
+	pterm.Info.Println("Good luck in school. You Got this 🎉")
+	pterm.Info.Println("Thanks for using canvas-sync ☺️. Star us on GitHub ⭐️: https://github.com/aidanaden/canvas-sync")
 }
